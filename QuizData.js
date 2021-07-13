a2e9a03fa61347c966055f2737595affc5f4b8f3
louay beltifa <louay.beltifa1@gmail.com>
2021-07-13 15:52:29 +0100

    add files

---
+++ QuizData.js
@@ -0,0 +1,92 @@
+const quizdata = [
+    {
+        question : '1. HTML stands for what?',
+        a:'Hypertrophic Management Language',
+        b:'Hypertext Markup Language',
+        c: 'Hyperresonant Marginal Logrithm',
+        d: 'Hyperberic Tertiary Logrithm',
+        correct:'b'
+    },
+    {
+        question : '2. Every HTML document should have what?',
+        a:'A head section, and within it a title, followed by a body',
+        b:'A title section, and within it a header, followed by a body',
+        c: 'A body and header',
+        d: 'None of these',
+        correct:'c'
+    },
+    {
+        question : '3. Which tag would insert an image into a Web page?',
+        a:'Img src=Glass.jpeg',
+        b:'P....Glass.jpeg..../p',
+        c: 'Img file=Glass.jpeg',
+        d: 'Picture=Glass.jpeg',
+        correct:'a'
+    },
+    {
+        question : '4. Use this tag to emphasize text.',
+        a:'A',
+        b:'M',
+        c: 'Em',
+        d: 'Tb',
+        correct:'c'
+    },
+    {
+        question : '5. What is the tag that begins a list of items?',
+        a:'Ol or ul',
+        b:'Ls',
+        c: 'Il',
+        d: 'Ul or ls',
+        correct:'a'
+    },
+    {
+        question : '6. What is the correct HTML tag for the largest heading?',
+        a:'Head',
+        b:'Heading',
+        c: 'H6',
+        d: 'H1',
+        correct:'d'
+    },
+    {
+        question : '7. How can you make a list that lists the items with numbers?',
+        a:'Ol',
+        b:'Dl',
+        c: 'Ul',
+        d: 'List',
+        correct:'a'
+    },
+    {
+        question : '8. Which tag would be used for a line break?',
+        a:'Lb',
+        b:'Br',
+        c: 'Brk',
+        d: 'Ln',
+        correct:'b'
+    },
+    {
+        question : '9. HTML program is saved using _______ extension.',
+        a:'.htl',
+        b:'.html',
+        c: '.hml',
+        d: '.htlm',
+        correct:'b'
+    },
+    {
+        question : '10. Which of the following is used increase the row height?',
+        a:'Cellspacing',
+        b:'Cellpadding',
+        c: 'Row span',
+        d: 'Col span',
+        correct:'c'
+    },
+    {
+        question : '11. The href attribute in the link tag specifies the:',
+        a:'Destination Of a link',
+        b:'link',
+        c: 'Hypertext',
+        d: 'None of the above',
+        correct:'a'
+    },
+
+];
+export { quizdata };
---
+++ script.js
@@ -0,0 +1,69 @@
+import { quizdata } from './QuizData.js';
+
+const quiz = document.getElementById("quiz");
+const answerEls = document.querySelectorAll(".answer");
+const questionEl = document.getElementById("question");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitBtn = document.getElementById("submit");
+
+let currentQuiz = 0;
+let score = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+    deselectAnswers();
+
+    const currentQuizData = quizdata[currentQuiz];
+
+    questionEl.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
+}
+
+function getSelected() {
+    let answer = undefined;
+
+    answerEls.forEach((answerEl) => {
+        if (answerEl.checked) {
+            answer = answerEl.id;
+        }
+    });
+
+    return answer;
+}
+
+function deselectAnswers() {
+    answerEls.forEach((answerEl) => {
+        answerEl.checked = false;
+    });
+}
+
+submitBtn.addEventListener("click", () => {
+    // check to see the answer
+    const answer = getSelected();
+
+    if (answer) {
+        if (answer === quizdata[currentQuiz].correct) {
+            score++;
+        }
+
+        currentQuiz++;
+        if (currentQuiz < quizdata.length) {
+            loadQuiz();
+        } else {
+            quiz.innerHTML = `
+                <h3>You answered correctly at ${score}/${quizdata.length} questions.</h3>
+                
+                <button onclick="location.reload()">Reload</button>
+            `;
+        }
+    }else {
+        alert("you need to chose an aswer !!")
+    }
+});
